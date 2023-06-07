db6fb651a7936afc11b0d55da096b240509906df
Jackson <Jackson.McDowell@gmail.com>
2023-06-06 21:09:51 -0600

    Final online map app before offline fork

--- app/src/main/java/com/example/doggyfindermapview/MainActivity.kt
+++ app/src/main/java/com/example/doggyfindermapview/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.doggyfindermapview
 
 import com.example.doggyfindermapview.R
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.UiSettings
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
-
 // Implement OnMapReadyCallback.
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, OnMarkerClickListener {
+    private val DOG = LatLng(-31.952854, 115.857342)
+    private var markerDog: Marker? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Set the layout file as the content view.
@@ -25,11 +31,31 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     // Get a handle to the GoogleMap object and display marker.
     override fun onMapReady(googleMap: GoogleMap) {
-        googleMap.addMarker(
+        markerDog?.tag = 0
+        markerDog = googleMap.addMarker(
             MarkerOptions()
-                .position(LatLng(0.0, 0.0))
-                .title("Marker")
+                .position(DOG)
+                .title("Dog Lat: " + DOG.latitude + ", Long: " + DOG.longitude)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
         )
+        googleMap.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker"))
+        googleMap.uiSettings.isZoomControlsEnabled = true
+        googleMap.uiSettings.isCompassEnabled = true
+        googleMap.uiSettings.isMyLocationButtonEnabled = true
+
+
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        // Display where the dog is at in lat long
+        val text = "Lat: " + marker.position.latitude + " Long: " + marker.position.longitude
+        val duration = Toast.LENGTH_LONG
+
+        val toast = Toast.makeText(this, text, duration) // in Activity
+        toast.show()
+
+
+        return false
     }
 
 
